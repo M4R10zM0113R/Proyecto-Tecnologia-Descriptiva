7957795b9a571e0e9948bc3e96491aed09b5ca34
M4R10zM0113R <Mario.moller15111994@hotmail.com>
2017-05-20 02:35:09 +0000

    Update Simon-dice.ino

--- Simon-dice/Simon-dice.ino
+++ Simon-dice/Simon-dice.ino
@@ -10,7 +10,6 @@ Archivo del programa Simón Dice
 
 // Declaración de posiciones en el Arduino.
 //// Entradas
-const int zumbador = 7;
 const int boton_rojo = 9;
 const int boton_azul = 10;
 const int boton_amarillo = 11;
@@ -22,3 +21,4 @@ const int led_azul = 3;
 const int led_amarillo = 4;
 const int led_verde = 5;
 const int led_random = 6;
+const int zumbador = 7;
