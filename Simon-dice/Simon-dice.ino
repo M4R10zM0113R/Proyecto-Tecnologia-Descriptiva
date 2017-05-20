ee35237cf434bd18805eaffbea8462c9998eb68c
M4R10zM0113R <Mario.moller15111994@hotmail.com>
2017-05-20 01:55:29 +0000

    Agregar constantes
    los puertos de entrada/salida usados

--- Simon-dice/Simon-dice.ino
+++ Simon-dice/Simon-dice.ino
@@ -7,3 +7,16 @@ Integrantes: (Nombre, Carnet)
 
 Archivo del programa Simón Dice
 */
+
+// Declaración de posiciones en el Arduino.
+//// Entradas
+const int led_rojo = 2;
+const int led_azul = 3;
+const int led_amarillo = 4;
+const int led_verde = 5;
+const int zumbador = 6;
+//// Salidas
+const int boton_rojo 9;
+const int boton_rojo 10;
+const int boton_rojo 11;
+const int boton_rojo 12;
