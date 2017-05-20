60b60a3f93b2cc8faf1c4577a7f8439d821770bb
M4R10zM0113R <Mario.moller15111994@hotmail.com>
2017-05-20 03:01:53 +0000

    Finalizar la declaración de variables

--- Simon-dice/Simon-dice.ino
+++ Simon-dice/Simon-dice.ino
@@ -8,7 +8,7 @@ Integrantes: (Nombre, Carnet)
 Archivo del programa Simón Dice
 */
 
-// Declaración de posiciones en el Arduino.
+// Declaración de posiciones en el Arduino
 //// Entradas
 const int boton_rojo = 9;
 const int boton_azul = 10;
@@ -22,3 +22,16 @@ const int led_amarillo = 4;
 const int led_verde = 5;
 const int led_random = 6;
 const int zumbador = 7;
+
+// Declaración de variables
+//// Zumbador
+int notas = 15; // Cantidad de notas en la melodía
+char melodia[] = "cacaffeggeeddc"; // Melodía, dada en notación musical GABCDEF
+int tiempos[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 4 }; // Tiempos por nota
+int tempo = 100; // Tiempo por nota
+//// Otros
+int puntuacion_maxima = 20; // Cantidad de pasos a recordar antes de ganar
+long secuencia[puntuacion_maxima]; // La variable para almacenar la secuencia
+int contador = 0; // Contador para recorrer la secuencia mientras se juegue
+int input = 5; // Para indicar el botón pulsado
+int despliegue = 500; // Tiempo en ms para desplegar cada LED en secuencia
