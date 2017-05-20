e1f3bf03371d75fe0311df0601999e066620f5d0
M4R10zM0113R <Mario.moller15111994@hotmail.com>
2017-05-20 02:29:30 +0000

    Comentarios erróneos, orden I/O
    puramente estilístico.

--- Simon-dice/Simon-dice.ino
+++ Simon-dice/Simon-dice.ino
@@ -10,15 +10,15 @@ Archivo del programa Simón Dice
 
 // Declaración de posiciones en el Arduino.
 //// Entradas
-const int led_rojo = 2;
-const int led_azul = 3;
-const int led_amarillo = 4;
-const int led_verde = 5;
-const int led_random = 6;
-//// Salidas
 const int zumbador = 7;
 const int boton_rojo = 9;
 const int boton_azul = 10;
 const int boton_amarillo = 11;
 const int boton_verde = 12;
 const int boton_random = 13;
+//// Salidas
+const int led_rojo = 2;
+const int led_azul = 3;
+const int led_amarillo = 4;
+const int led_verde = 5;
+const int led_random = 6;
